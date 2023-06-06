d56350b18b481c89ec802ecb6906c4f7d18e5d1e
Morozzzuk <115870040+Morozzzuk@users.noreply.github.com>
2023-06-06 23:46:33 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,21 @@ class App extends Component {
     filter: '',
   };
 
+  // componentDidMount() {
+  //   const contacts = localStorage.getItem('contacts');
+  //   const parsedContacts = JSON.parse(contacts);
+  //   if (parsedContacts) {
+  //     this.setState({ contacts: parsedContacts });
+  //   }
+  // }
+
+  // componentDidUpdate(_, prevState) {
+  //   const { contacts } = this.state;
+  //   if (contacts !== prevState.contacts) {
+  //     localStorage.setItem('contacts', JSON.stringify(contacts));
+  //   }
+  // }
+
   addContact = ({ name, number }) => {
     const normalizedName = name.toLowerCase();
     const { contacts } = this.state;
